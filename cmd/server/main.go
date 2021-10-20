44cfcb04987cb0c1f76d345f1d2a6b23c2833e13
Jose Campos <zecampos2015@yahoo.com.br>
2021-10-20 15:31:17 -0300

    feat: Added health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/zecampos/go-rest-api/internal/transport/http"
+)
 
 // App - the struct which contains things like pointes
 // to database connections
@@ -8,6 +13,12 @@ type App struct{}
 
 func (app *App) Run() error {
 	fmt.Println("Setting Up Our App")
+	handler := transportHTTP.NewHandler()
+	handler.SetupROutes()
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
 	return nil
 }
 
---
+++ internal/transport/http/handler.go
@@ -0,0 +1,28 @@
+package http
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Handler - stores pointer to our comments services
+type Handler struct {
+	Router *mux.Router
+}
+
+// NewHandler - returns a pointer to a handler
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+// SetupRoutes - sets up all the routes for our aplicattion
+
+func (h *Handler) SetupROutes() {
+	fmt.Println("Setting up Routes")
+	h.Router = mux.NewRouter()
+	h.Router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "I am Alive")
+	})
+}
