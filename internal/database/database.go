6bf82d7957b985b9633086d27d1336bef07bbe0a
Jose Campos <zecampos2015@yahoo.com.br>
2021-10-25 09:57:40 -0300

    chore: add database connection

---
+++ internal/database/database.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+func NewDatabase() (*gorm.DB, error) {
+	fmt.Println("Stting Up new database connection")
+
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbTable := os.Getenv("DB_TABLE")
+	dbPort := os.Getenv("DB_PORT")
+
+	// dbConnectionString := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + strconv.Itoa(dbPort) + ")/" + dbTable
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", dbHost, dbPort, dbUsername, dbTable, dbPassword)
+	fmt.Println(connectionString)
+
+	db, err := gorm.Open("postgres", connectionString)
+	if err != nil {
+		return db, err
+	}
+
+	if err := db.DB().Ping(); err != nil {
+		return db, err
+	}
+
+	return db, nil
+}
